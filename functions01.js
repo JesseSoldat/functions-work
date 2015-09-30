4ec34166e1b8d329482f56680686254c4a34801d
Jesse Soldat <jsoldat@hotmail.com>
2015-09-30 12:54:09 -0400

    finished functions01.js

--- functions01.js
+++ functions01.js
@@ -23,23 +23,30 @@
 // Q1
 // Write a function called `sum` that takes two
 // parameters and returns the sum of those 2 numbers.
+var sum = function(num1, num2) {
+   return num1 + num2;
+};
 
-
-
+sum(5,7);
 
 // Q2
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
 
-
-
+var avg = function(num1, num2, num3) {
+    var total = num1 + num2 + num3;
+     return total / 3;
+};
+avg(2,3,4);
 
 // Q3
 // Write a function called `getLength` that takes one
 // parameter (a string) and returns the length
+var getLength = function(string) {
+  return string.length;
+};
 
-
-
+getLength('My name is the the Flash');
 
 // Q4
 // Write a function called `greaterThan` that takes
@@ -47,17 +54,20 @@
 // parameter is greater than the first. Otherwise
 // the function should return `false`.
 
-
-
+var greaterThan = function(a,b) {
+          return a < b;
+}; 
+greaterThan(7,9);
 
 // Q5
 // Write a function called `greet` that takes a
 // single parameter and returns a string that
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
-
-
-
+var greet = function(Name) {
+  return 'Hello, ' + Name+'!';
+};
+greet('Jesse');
 
 // Q6
 // Write a function called `madlib` that takes
@@ -69,8 +79,10 @@
 // strings. For example:
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
-
-
+var madlib = function(adverb,adjective1,noun,adjective2) {
+        return 'The '+adverb+' '+adjective1+' '+noun+' is '+adjective2+'.';
+};
+madlib('very','fat','cat','sleepy');
 
 
 ///////////////////////////////////////////////////////
